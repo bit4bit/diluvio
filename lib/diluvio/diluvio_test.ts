3a97b2e197e4e479675010ae6c90a2dfd684d354
bit4bit <>
2021-04-03 02:53:41 +0000

    feat: add publisher
    FossilOrigin-Name: a7df4bfb2df91bfc6f028edaaad20c099ec4126881a6139fc816bfae9cf752b2

--- lib/diluvio/diluvio_test.ts
+++ lib/diluvio/diluvio_test.ts
@@ -4,42 +4,65 @@ import {
     assertThrows
 } from 'https://deno.land/std@0.88.0/testing/asserts.ts'
 
-import { Diluvio, DialplanFetcher, FreeswitchConnectioner } from './mod.ts'
+import { Diluvio, DialplanFetcher, Publisher, FreeswitchConnectioner } from './mod.ts'
+import type { FreeswitchEvent, FreeswitchEventCallback } from './mod.ts'
 
 class FreeswitchConnectionFake implements FreeswitchConnectioner {
     public actions: Array<string> = []
+    public hangups_cb: Array<FreeswitchEventCallback> = []
     
-    answer() {
+    async answer() {
         this.actions.push('answer')
-        return null
     }
 
-    execute(cmd: string) {
+    async execute(cmd: string) {
         this.actions.push(`execute: ${cmd}`)
         return null
     }
     
-    api(cmd: string, arg: string) {
+    async api(cmd: string, arg: string) {
         this.actions.push(`api: ${cmd} ${arg}`)
         return null
+    }
+
+    async hangup(reason: string) {
+        for(const cb of this.hangups_cb) {
+            cb(new Map())
+        }
+    }
+    
+    async on_hangup(cb: FreeswitchEventCallback) {
+        this.hangups_cb.push(cb)
     }
 }
 
 class DialplanFetchFake implements DialplanFetcher {
     fetch(url: string) {
         return [
+            {action: 'parameter', data: {name: 'on_hangup', publish: 'http://localhost'}},
             {action: 'answer'},
-            {action: 'echo'}
+            {action: 'echo'},
+            {action: 'hangup'}
         ]
+    }
+}
+
+class PublishFake implements Publisher {
+    public actions: Array<string> = []
+        
+    async response(destination: string, event: FreeswitchEvent) {
+        this.actions.push(`to: ${destination}`)
     }
 }
 
 Deno.test('iteration 1 outbound', async () => {
     const fsconn = new FreeswitchConnectionFake()
     const dialplanFetch = new DialplanFetchFake()
+    const publish = new PublishFake()
     
-    const conn = Diluvio.connect(fsconn, dialplanFetch)
-    await conn.process()
+    const diluvio = new Diluvio(dialplanFetch, publish)
+    await diluvio.connect(fsconn).process()
 
     assertEquals(fsconn.actions, ['answer', 'execute: echo'])
+    assertEquals(publish.actions, ['to: http://localhost'])
 })
--- lib/diluvio/mod.ts
+++ lib/diluvio/mod.ts
@@ -1,11 +1,18 @@
+export type FreeswitchEvent = Map<string, string>
+
+export type FreeswitchEventCallback = (event: FreeswitchEvent) => void
 
 export interface FreeswitchConnectioner {
-    answer(): void
-    execute(cmd: string, arg: string): null | string
-    api(cmd: string, arg: string): null | string
+    answer(): Promise<void>
+    execute(cmd: string, arg: string): Promise<null | string>
+    api(cmd: string, arg: string): Promise<null | string>
+
+    hangup(reason: string): Promise<void>
+    on_hangup(cb: FreeswitchEventCallback): Promise<void>
 }
 
-export type DialplanActioner = {action: string}
+type DialplanActionerParameter = {name: string, publish: string}
+export type DialplanActioner = {action: string, data?: DialplanActionerParameter | string} 
 
 export interface DialplanFetcher {
     fetch(url: string): Array<DialplanActioner> | []
@@ -15,13 +22,19 @@ export interface DiluvioConnectioner {
     process(): Promise<void>
 }
 
+export interface Publisher {
+    response(destination: string, event: FreeswitchEvent): Promise<void>
+}
+
 class DiluvioConnection {
     private fsconn: FreeswitchConnectioner
     private dialplan: DialplanFetcher
+    private publish: Publisher
     
-    constructor(fsconn: FreeswitchConnectioner, dialplanFetcher: DialplanFetcher) {
+    constructor(fsconn: FreeswitchConnectioner, dialplanFetcher: DialplanFetcher, publish: Publisher) {
         this.fsconn = fsconn
         this.dialplan = dialplanFetcher
+        this.publish = publish
     }
     
     async process() {
@@ -30,19 +43,43 @@ class DiluvioConnection {
         for(const plan of dialplan) {
             switch(plan.action) {
                 case 'answer':
-                    this.fsconn.answer()
+                    await this.fsconn.answer()
                     break
                 case 'echo':
-                    this.fsconn.execute('echo', '')
+                    await this.fsconn.execute('echo', '')
                     break
+                case 'hangup':
+                    await this.fsconn.hangup(plan.data as string ?? 'NORMAL_CLEARING')
+                case 'parameter':
+                    if (!plan.data) {
+                        console.info(`omiting parameter ${JSON.stringify(plan)}`)
+                        continue
+                    }
+                    const param = plan.data as DialplanActionerParameter
+                    switch(param.name) {
+                        case 'on_hangup':
+                            this.fsconn.on_hangup((event) => {
+                                this.publish.response(param.publish, event)
+                            })
+                            break
+                        default:
+                            throw new Error(`parameter unkown handler ${param.name}`)
+                    }
             }
         }
     }
 }
 
 export class Diluvio {
-
-    static connect(fsconn: FreeswitchConnectioner, dialplanFetcher: DialplanFetcher): DiluvioConnectioner {
-        return new DiluvioConnection(fsconn, dialplanFetcher)
+    private dialplanFetcher: DialplanFetcher
+    private publish: Publisher
+    
+    constructor(dialplanFetcher: DialplanFetcher, publish: Publisher) {
+        this.dialplanFetcher = dialplanFetcher
+        this.publish = publish
+    }
+    
+    connect(fsconn: FreeswitchConnectioner): DiluvioConnectioner {
+        return new DiluvioConnection(fsconn, this.dialplanFetcher, this.publish)
     }
 }
