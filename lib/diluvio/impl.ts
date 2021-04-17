f422b99fec1dbb0c31365cd9499c84e5e9987b42
bit4bit <>
2021-04-17 21:23:13 +0000

    feat: first implementacion freeswitch outbound connector
    FossilOrigin-Name: a3f7826a1e3bdbc6278798ae9f05dac2873bd25caa2351f3204f0212a6273d68

--- lib/diluvio/impl.ts
+++ lib/diluvio/impl.ts
@@ -1,4 +1,19 @@
-import { FreeswitchOutboundConnectioner } from './mod.ts'
+import {
+    FreeswitchOutboundConnectioner,
+    FreeswitchEvent,
+    FreeswitchCommandReply,
+    FreeswitchApiReply
+} from './mod.ts'
+
+import {
+    BufReader,
+    Buffer,
+    StringReader
+} from 'https://deno.land/std@0.93.0/io/mod.ts'
+
+import {
+    TextProtoReader
+} from 'https://deno.land/std@0.93.0/textproto/mod.ts'
 
 export interface PduOptions {
     command: string
@@ -6,8 +21,13 @@ export interface PduOptions {
     arg?: string
 }
 
-export class Pdu {
-    static async writeTo(opts: PduOptions, w: Deno.Writer) {
+interface Pdu {
+    kind: string
+    data:  any
+}
+
+export class Message {
+    static async writeTo(w: Deno.Writer, opts: PduOptions) {
         const pdu = [`sendmsg
 call-command: ${opts.command}
 execute-app-name: ${opts.app}`]
@@ -17,7 +37,122 @@ execute-app-name: ${opts.app}`]
         pdu.push("\n")
         
         const data = new TextEncoder().encode(pdu.join("\n"))
-        await w.write(data)
+        const n = await w.write(data)
+        if (n != data.length)
+            throw new Error('mismatch data length')
     }
 }
 
+type FreeswitchCallbackEvent = (event: any) => void
+
+export class FreeswitchOutboundTCP {
+    private conn: any
+    private reader: BufReader
+    private cb_events: Array<FreeswitchCallbackEvent>
+    private text_encoder: TextEncoder
+    private text_decoder: TextDecoder
+    private alive: boolean = true
+    
+    constructor(conn: Deno.Reader & Deno.Writer) {
+        this.conn = conn
+        this.reader = new BufReader(conn)
+        this.cb_events = []
+        this.text_encoder = new TextEncoder()
+        this.text_decoder = new TextDecoder()
+    }
+
+    on_event(cb: any) {
+        this.cb_events.push(cb)
+    }
+
+    async processOne() {
+        const pdu = await this.read()
+        switch(pdu.kind) {
+            case 'event':
+                for(const cb of this.cb_events) {
+                    cb(pdu.data)
+                }
+                break
+            default:
+                throw new Error('not know how handle pdu')
+        }
+    }
+    
+    async process() {
+        await this.conn.write(this.text_encoder.encode("connect\n\n"))
+
+        while(this.alive) {
+            await this.processOne()
+        }
+    }
+
+    close() {
+        this.alive = false
+    }
+    
+    private async read(): Promise<Pdu> {
+        const head: any = await this.read_head(this.reader)
+        const body: string | null = await this.read_body(head)
+
+        const content_type = head['content-type']
+
+        switch(content_type) {
+            case 'text/event-plain':
+                if (body) {
+                    const buff = new BufReader(new StringReader(body))
+                    const header = await this.read_head(buff)
+                    return {kind: 'event', data: header}
+                } else {
+                    return {kind: 'event', data: {}}
+                }
+            default:
+                throw new Error(`not implemented ${content_type}`)
+        }
+    }
+
+    private async read_head(buff: BufReader) {
+        const head: any = {}
+        
+        while (true) {
+            const result = await buff.readLine()
+            if (result === null)
+                break
+
+            const { line, more } = result
+            if (more)
+                throw new Error('not handle when have more on line')
+            
+            const sline: string = this.text_decoder.decode(line)
+
+            if (sline == '')
+                break
+
+            const [key, value] = sline.split(':')
+            head[key.toLowerCase()] = value.trim()
+
+            const peek = await buff.peek(1)
+            if (peek !== null && peek[0] == 10) {
+                const eol = new Uint8Array(1)
+                await buff.read(eol)
+                break
+            }
+        }
+
+        return head
+    }
+    
+    private async read_body(head: any): Promise<string | null> {
+        const content_length: number = parseInt(head['content-length'])
+
+        if (content_length > 0) {
+            console.log(`reading body ${content_length}`)
+            const body = new Uint8Array(content_length)
+            const n = await this.reader.read(body) ?? 0
+            if (content_length != n)
+                throw new Error(`mismatch content-length ${content_length} of data readed ${n}`)
+            return this.text_decoder.decode(body)
+        }
+
+        return null
+    }
+}
--- lib/diluvio/impl_test.ts
+++ lib/diluvio/impl_test.ts
@@ -5,16 +5,16 @@ import {
 } from 'https://deno.land/std@0.88.0/testing/asserts.ts'
 
 import {
-    StringWriter
-} from "https://deno.land/std@0.88.0/io/mod.ts";
+    StringWriter,
+    Buffer,
+} from "https://deno.land/std@0.93.0/io/mod.ts";
 
-import { Pdu } from './impl.ts'
-
+import { Message, FreeswitchOutboundTCP } from './impl.ts'
 
 Deno.test('sendmsg without arg', async () => {
     const out = new StringWriter()
     
-    await Pdu.writeTo({command: 'execute', app: 'hangup'}, out)
+    await Message.writeTo(out, {command: 'execute', app: 'hangup'})
     assertEquals(out.toString(), `sendmsg
 call-command: execute
 execute-app-name: hangup
@@ -25,11 +25,68 @@ execute-app-name: hangup
 Deno.test('sendmsg with arg', async () => {
     const out = new StringWriter()
     
-    await Pdu.writeTo({command: 'execute', app: 'playback', arg: '/tmp/test.ogg'}, out)
+    await Message.writeTo(out, {command: 'execute', app: 'playback', arg: '/tmp/test.ogg'})
     assertEquals(out.toString(), `sendmsg
 call-command: execute
 execute-app-name: playback
 execute-app-arg: /tmp/test.ogg
 
 `)
+})
+
+Deno.test('dispatch event', async () => {
+    const listener = await Deno.listen({port: 9090})
+   
+    const tcp_conn: Deno.Conn = await Deno.connect({port: 9090})
+    const conn = new FreeswitchOutboundTCP(tcp_conn)
+    const event = `Content-Length: 555
+Content-Type: text/event-plain
+
+Event-Name: RE_SCHEDULE
+Core-UUID: a95e64ec-df1f-48f4-acbf-34b4c359d747
+FreeSWITCH-Hostname: faulty.local
+FreeSWITCH-Switchname: faulty.local
+FreeSWITCH-IPv4: 192.168.1.102
+FreeSWITCH-IPv6: %3A%3A1
+Event-Date-Local: 2015-07-04%2015%3A18%3A14
+Event-Date-GMT: Sat,%2004%20Jul%202015%2018%3A18%3A14%20GMT
+Event-Date-Timestamp: 1436033894106095
+Event-Calling-File: switch_scheduler.c
+Event-Calling-Function: switch_scheduler_execute
+Event-Calling-Line-Number: 71
+Event-Sequence: 1996
+Task-ID: 2
+Task-Desc: heartbeat
+Task-Group: core
+Task-Runtime: 1436033914
+
+`
+    // prelude of connection with event
+    const arrangeListen = new Promise(async (resolve, reject) => {
+        const conn = await listener.accept()
+        const connect = new Uint8Array(9)
+        await conn.read(connect)
+
+        const data = new TextEncoder().encode(event)
+        const n = await conn.write(data)
+        assertEquals(n, data.length)
+        conn.close()
+    })
+
+    const pconn = conn.process()
+
+    const wait_event = new Promise((resolve) => {
+        conn.on_event((event: any) => {
+            resolve(event)
+            conn.close()
+            tcp_conn.close()
+            listener.close()
+        })
+    })
+    
+
+    
+    const expected_event: any = await wait_event
+    assertEquals(expected_event['event-name'], 'RE_SCHEDULE')
+    await pconn
 })
