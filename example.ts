047a7ca9b6d49ae36b7e3caaa55848d064ea0b14
bit4bit <>
2021-05-02 23:09:52 +0000

    doc: comment example.ts
    FossilOrigin-Name: 88c42c07909c119d216b074870e0efde0c5ff29c26c5a8cbaafd422f2f27406d

--- example.ts
+++ example.ts
@@ -1,3 +1,4 @@
+//example of usage
 import { FreeswitchOutboundTCP, DialplanHTTP, PublishHTTP } from './lib/diluvio/impl.ts'
 import { Diluvio } from './lib/diluvio/mod.ts'
 import { text_decoder } from './lib/deps.ts'
@@ -7,7 +8,8 @@ import { serve } from "https://deno.land/std@0.95.0/http/server.ts";
 const dialplan = new DialplanHTTP('http://localhost:43001')
 const publish = new PublishHTTP('http://localhost:43001')
 
-//example of dialplan
+
+// web server export dialplan
 const server = serve({port: 43001})
 new Promise(async (resolve) => {
     for await (const req of server) {
@@ -15,21 +17,36 @@ new Promise(async (resolve) => {
 
         console.log(req.url)
         switch(req.url) {
+                // get and process events
             case '/event':
                 const event: any = JSON.parse(text_decoder.decode(body))
                 console.log(event['event-name'])
                 if (event['event-name'] == 'CHANNEL_DATA') {
                     console.log(event)
                 }
+                // server query main dialplan
             case '/':
-                const plan = [{parameter: 'on_hangup', value: '/hangup'},
-                              {parameter: 'on_event', value: '/event'},
-                              {action: 'answer'},
-                              {api: 'uptime', reply: '/uptime'},
-                              {action: 'echo'},
-                              {action: 'hangup'}]
+                const plan = [
+                    // configure url for handling hangup
+                    {parameter: 'on_hangup', value: '/hangup'},
+                    // configure url for handling events
+                    {parameter: 'on_event', value: '/event'},
+
+                    // run action and notify next dialplan
+                    {action: 'answer', diaplan: '/continue-dialplan'},
+
+                ]
                 req.respond({body: JSON.stringify(plan)})
                 break
+                // execute after answer done
+            case '/continue-dialplan':
+                const continue_plan = [
+                    {api: 'uptime', reply: '/uptime'},
+                    {action: 'echo'},
+                    {action: 'hangup'}
+                ]
+                req.respond({body: JSON.stringify(continue_plan)})
+                // get api response
             case '/uptime':
                 console.log('uptime:')
                 console.log(text_decoder.decode(body))
@@ -43,11 +60,13 @@ new Promise(async (resolve) => {
 
 
 //run main logic
+
 const listener = Deno.listen({port: 43000})
 console.log('listening on 0.0.0.0:43000')
 
 for await (const conn of listener) {
     new Promise(async (resolve) => {
+        // handshake freeswitch and process
         const diluvio = new Diluvio(dialplan, publish)
         const fsconn = new FreeswitchOutboundTCP(conn)        
         const diluvio_conn = diluvio.connect(fsconn)
